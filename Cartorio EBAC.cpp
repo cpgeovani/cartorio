14587971f9511290018d711c9908846c93db6df6
cpgeovani <geovanipiaceskicardoso19@gmail.com>
2024-06-28 19:40:34 -0300

    Incluso uma nova funcao
    funcao nova adicionada, opcao de sair para o usuario

--- Cartorio EBAC.cpp
+++ Cartorio EBAC.cpp
@@ -131,7 +131,8 @@ int main()
     printf ("\t1 - Registrar nomes \n");
 	printf ("\t2 - Consultar nomes \n");
 	printf ("\t3 - Deletar nomes \n\n");
-    printf("Opcao: ");
+	printf ("\t4 - Sair do sistema\n\n");
+    printf ("Opcao: ");
    
     scanf("%d", &opcao);
    
@@ -153,6 +154,11 @@ int main()
     deletar();
     break;
     
+    case 4:
+    printf("Obrigado por utilizar o sistema!\n");
+    return 0;
+    break;
+         
     default:
     printf("Essa opcao nao esta disponivel ! \n");
     system("pause");
